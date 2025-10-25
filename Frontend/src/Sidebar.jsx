67cf8e3d5e38338430172b0a68da7e2b57fcdbd1
Manjot-git <manjott13k@gmail.com>
2025-10-25 19:47:44 +0530

    final project

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080"  // remove /api here
-    : VITE_API_URL;
+    : import.meta.env.VITE_API_URL;
 
 function Sidebar(){
     const navigate = useNavigate();  
